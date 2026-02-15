ba562b12ca1bd2deb8b0f5a052f4823bc77fca0e
Ashfaq Salehin <ashfaq.salehin1701@gmail.com>
2026-02-15 22:40:18 +0000

    Fix

--- index.py
+++ index.py
@@ -6,3 +6,7 @@ MAX_WALK_LENGTH=80
 TEMPORAL_DATASET_PATH=f"{BASE_PATH}/temporal"
 THUNDERRW_DATASET_PATH=f"{BASE_PATH}/static_preprocessed_thunderrw"
 FLOWWALKER_DATASET_PATH=f"{BASE_PATH}/static_preprocessed_flowwalker"
+
+TEMPEST_OUTPUT_FILE=f"{BASE_PATH}/tempest_walks.txt"
+THUNDERRW_OUTPUT_FILE=f"{BASE_PATH}/thunderrw_walks.txt"
+FLOWWALKER_OUTPUT_FILE=f"{BASE_PATH}/flowwalker_walks.txt"
