636cd58a4b71adbe4f882fca27a71da622733b5d
Ashfaq Salehin <ashfaq.salehin1701@gmail.com>
2026-02-15 20:57:20 +0000

    First commit

---
+++ index.py
@@ -0,0 +1,8 @@
+BASE_PATH='/mnt/lustre/users/inf/ms2420/non-temporal-comparison-datasets'
+
+NUM_WALKS=10_000_000
+MAX_WALK_LENGTH=80
+
+TEMPORAL_DATASET_PATH=f"{BASE_PATH}/temporal"
+THUNDERRW_DATASET_PATH=f"{BASE_PATH}/static_preprocessed_thunderrw"
+FLOWWALKER_DATASET_PATH=f"{BASE_PATH}/static_preprocessed_flowwalker"
